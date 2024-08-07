20c6be0df0ebd92809592b46c851ead3627a5b3b
mohitkr2809 <mohitkrmnm282003@gmail.com>
2024-08-07 23:37:20 +0530

    Variables to summary of datatypes

---
+++ video 1/01_variables.js
@@ -0,0 +1,20 @@
+const accountId =144553
+let accountEmail ="hitesh@google.com"
+var accountpassword ="12345"
+accountcity ="Jaipur"
+let accountState;
+// account = 2 // not allowed
+
+accountEmail ="hc@hc.com"
+accoontpassword ="212121221"
+accountCity ="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountpassword,accountCity])
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
